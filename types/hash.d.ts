ce5b2d4cd624f9674a9cb2ba0fd0c8328a1da1cf
Dennis Richter <richter.dennis@live.de>
2026-02-06 16:52:03 +0100

    Improve hash functions and types

--- types/hash.d.ts
+++ types/hash.d.ts
@@ -1,39 +1,24 @@
 import { BinaryToTextEncoding, HashOptions } from 'node:crypto';
 
-declare function hash(
+type HashResult<E extends BinaryToTextEncoding | null | undefined> =
+	E extends 'base64' | 'base64url' | 'hex' | undefined ? string : Buffer;
+
+type HashFn = <E extends BinaryToTextEncoding | null | undefined = undefined>(
+	value: string | Buffer,
+	options?: HashOptions & { encoding?: E }
+) => HashResult<E>;
+
+declare function hash<E extends BinaryToTextEncoding | null | undefined = undefined>(
 	algorithm: string,
 	value: string | Buffer,
-	options?: HashOptions & { encoding?: BinaryToTextEncoding | null }
-): string | Buffer;
+	options?: HashOptions & { encoding?: E }
+): HashResult<E>;
 
-export declare const md5: (
-	value: string | Buffer,
-	options?: HashOptions & { encoding?: BinaryToTextEncoding | null }
-) => string | Buffer;
-
-export declare const sha1: (
-	value: string | Buffer,
-	options?: HashOptions & { encoding?: BinaryToTextEncoding | null }
-) => string | Buffer;
-
-export declare const sha224: (
-	value: string | Buffer,
-	options?: HashOptions & { encoding?: BinaryToTextEncoding | null }
-) => string | Buffer;
-
-export declare const sha256: (
-	value: string | Buffer,
-	options?: HashOptions & { encoding?: BinaryToTextEncoding | null }
-) => string | Buffer;
-
-export declare const sha384: (
-	value: string | Buffer,
-	options?: HashOptions & { encoding?: BinaryToTextEncoding | null }
-) => string | Buffer;
-
-export declare const sha512: (
-	value: string | Buffer,
-	options?: HashOptions & { encoding?: BinaryToTextEncoding | null }
-) => string | Buffer;
+export declare const md5: HashFn;
+export declare const sha1: HashFn;
+export declare const sha224: HashFn;
+export declare const sha256: HashFn;
+export declare const sha384: HashFn;
+export declare const sha512: HashFn;
 
 export default hash;
